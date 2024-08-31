53b6342ebfb78dda3cc336124983e80c7ea0d99f
deNeedo <jkudrys@protonmail.com>
2024-08-31 23:30:31 +0200

    Added support for fetching player stats from the server. Modified scenes structure.

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -2,72 +2,59 @@ using System;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 public class NetworkManager : MonoBehaviour
 {
     private static TcpClient client;
     private static NetworkStream stream;
-    public static string currentPlayer = null;
-    private static bool Connect()
-    {
-        try
-        {
+    private static bool Connect() {
+        try {
             NetworkManager.client = new TcpClient("10.147.17.201", 54429);
             NetworkManager.stream = client.GetStream();
             return true;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Debug.LogError("Socket error: " + e.Message);
             return false;
         }
     }
-    public static void Login(string username, string password)
-    {
+    public static void Login(string username, string password) {
         bool flag = NetworkManager.Connect();
-        if (flag == true)
-        {
-            currentPlayer = username;
+        if (flag == true) {
             string message = "LOGIN " + username + " " + password + "\n";
             byte[] data = Encoding.UTF8.GetBytes(message);
             NetworkManager.stream.Write(data, 0, data.Length);
         }
     }
-    public static void Register(string username, string password)
-    {
+    public static void Register(string username, string password) {
         bool flag = NetworkManager.Connect();
-        if (flag == true)
-        {
+        if (flag == true) {
             string message = "REGISTER " + username + " " + password + "\n";
             byte[] data = Encoding.UTF8.GetBytes(message);
             NetworkManager.stream.Write(data, 0, data.Length);
         }
     }
-    public static string GetPlayer() {return currentPlayer;}
-    public static void FetchStats(string player)
-    {
-
+    public static void Fetch(string username) {
+        bool flag = NetworkManager.Connect();
+        if (flag == true) {
+            string message = "FETCH " + username + "\n";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            NetworkManager.stream.Write(data, 0, data.Length);
+        }
     }
-    void Update()
-    {
-        if (stream != null && stream.DataAvailable)
-        {
+    void Update() {
+        if (stream != null && stream.DataAvailable) {
             byte[] receivedData = new byte[client.Available];
             NetworkManager.stream.Read(receivedData, 0, receivedData.Length);
             string message = Encoding.UTF8.GetString(receivedData);
-            Scene currentScene = SceneManager.GetActiveScene();
-            Debug.Log(message);
-            if (message.Contains("0") && currentScene.name == "00_Login")
-            {
-                ChangeScreen.ChangeScene("01_Profile");
-            }
-            else if (message.Contains("0") && currentScene.name == "00_Register")
-            {
-                ChangeScreen.ChangeScene("00_Login");
-            }
-            else if (message.Contains("1") && currentScene.name == "00_Login")
-            {
-                currentPlayer = null;
+            string[] temp = message.Split(' ');
+            if (message.Contains("LOGIN 0") && GameManager.current_scene == "00_Login") {
+                NetworkManager.Fetch(temp[2]);
+                PlayerManager.Set(temp[2]);
+                GameManager.ChangeScene("01_Profile");
+            } else if (message.Contains("REGISTER 0") && GameManager.current_scene == "00_Register") {
+                GameManager.ChangeScene("00_Login");
+            } else if (message.Contains("FETCH 0")) {
+                temp = temp[2].Split('\t');
+                PlayerManager.Set(0, int.Parse(temp[0])); PlayerManager.Set(1, int.Parse(temp[1])); PlayerManager.Set(2, int.Parse(temp[2]));
             }
         }
     }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,31 +1,47 @@
 using UnityEngine;
-public class PlayerManager : MonoBehaviour
-{
-    public static PlayerManager Instance {get; private set;}
-    private string _player;
-    public string Player {
-        get {return _player;}
-        set {_player = value;}
+public class PlayerManager : MonoBehaviour {
+    private static string username = null;
+    private static int trait0 = 0;
+    private static int trait1 = 0;
+    private static int trait2 = 0;
+    public static void Set(string username) {
+        PlayerManager.username = username;
+        Debug.Log("Set to: " + username);
     }
-    private int _fitness;
-    public int Fitness {
-        get {return _fitness;}
-        set {_fitness = value;}
-    }
-    // define more traits in the same way
-    private void Awake() {
-        if (Instance == null) {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        } else {
-            Destroy(gameObject);
+    public static void Set(int trait, int value) {
+        switch (trait) {
+            case 0:
+                PlayerManager.trait0 = value;
+                Debug.Log("Trait0 set to: " + value);
+                break;
+            case 1:
+                PlayerManager.trait1 = value;
+                Debug.Log("Trait1 set to: " + value);
+                break;
+            case 2:
+                PlayerManager.trait2 = value;
+                Debug.Log("Trait2 set to: " + value);
+                break;
         }
     }
-    private void OnLoad() {
-        // somehow fetch player info from server
-        Player = NetworkManager.GetPlayer();
-        Fitness = 10;
-        // set all traits, items etc
-        Debug.Log(Player);
+    public static int Get(int trait) {
+        switch (trait) {
+            case 0:
+                return trait0;
+                break;
+            case 1:
+                return trait1;
+                break;
+            case 2:
+                return trait2;
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+    public static void Reset() {
+        PlayerManager.username = null;
+        Debug.Log("Reset");
     }
 }
