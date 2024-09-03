b960843bfbac95feb64c3221cfa77fa2bed317ac
odzioo123 <1konrad.sadowski@gmail.com>
2024-09-03 15:11:07 +0200

    Deleted listeners, put EnduranceManager to SceneManager, deleted TextStarHandler and PlayerStats

--- Assets/Scripts/Scene7/EnduranceManager.cs
+++ Assets/Scripts/Scene7/EnduranceManager.cs
@@ -6,63 +6,52 @@ public class EnduranceManager : MonoBehaviour
     public Slider enduranceSlider;
     public Text enduranceText;
     public Text feedbackText;
-    public Button relaxButton;
     public Text buttonText;
-
-    private int endurance;
-    private int stars;
-    private int relaxSessions;
+    public Text starsText;
 
     private void Start()
     {
-        endurance = PlayerManager.GetEndurance();
-        stars = PlayerManager.GetStars();
-        relaxSessions = PlayerManager.GetRelaxSessions();
-
-        enduranceSlider.value = endurance;
+        enduranceSlider.value = PlayerManager.GetEndurance();
         UpdateEnduranceText();
         UpdateButtonText();
         UpdateFeedbackText("1 Star for Relax Session");
-
-        // Add a listener to the button click
-        relaxButton.onClick.AddListener(OnRelaxButtonClicked);
+        UpdateStarsText();
     }
 
     private void UpdateEnduranceText() {
-        enduranceText.text = endurance.ToString();
+        enduranceText.text = PlayerManager.GetEndurance().ToString();
     }
     private void UpdateFeedbackText(string message) {
         feedbackText.text = message;
     }
     private void UpdateButtonText() {
-        buttonText.text = "Relax " + (10 - relaxSessions) + "/10";
+        buttonText.text = "Relax " + (10 - PlayerManager.GetRelaxSessions()) + "/10";
     }
-
-    private void OnRelaxButtonClicked() {
-        if (stars > 0 && relaxSessions > 0 && endurance <= 80) {
+    private void UpdateStarsText() {
+        starsText.text = "Stars: " + PlayerManager.GetStars();
+    }
+    public void OnRelaxButtonClicked() {
+        if (PlayerManager.GetStars() > 0 && PlayerManager.GetRelaxSessions() > 0 && PlayerManager.GetEndurance() <= 80) {
             bool success = NetworkManager.UseRelaxSession(PlayerManager.Get());
             if (success) {
-                endurance = PlayerManager.GetEndurance();
-                stars = PlayerManager.GetStars();
-                relaxSessions = PlayerManager.GetRelaxSessions();
-
-                enduranceSlider.value = endurance;
+                enduranceSlider.value = PlayerManager.GetEndurance();
                 UpdateEnduranceText();
                 UpdateButtonText();
                 UpdateFeedbackText("Endurance increased!");
+                UpdateStarsText();
             }
             else {
                 UpdateFeedbackText("Failed to use relax session. Please try again.");
             }
         }
         else {
-            if (stars <= 0) {
+            if (PlayerManager.GetStars() <= 0) {
                 UpdateFeedbackText("Not enough stars to use relax session.");
             }
-            else if (relaxSessions <= 0) {
+            else if (PlayerManager.GetRelaxSessions() <= 0) {
                 UpdateFeedbackText("Not enough relax sessions available.");
             }
-            else if (endurance > 80) {
+            else if (PlayerManager.GetEndurance() > 80) {
                 UpdateFeedbackText("Endurance is already high. Cannot increase further.");
             }
         }
